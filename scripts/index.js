2aa0ec92ba79dd95c705b917ae553eefd76c8522
PowerPlayHockey <polarishockey@gmail.com>
2018-11-19 12:43:31 -0600

    populating drop down with companies

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,34 @@ console.log('js');
 $(document).ready(function() {
 
 
+    //import guest info
     let guestsArr = $.getJSON("scripts/Guests.json", function( data ){
         console.log('guest data:', data);
     });
 
-    let companiesArr = $.getJSON("scripts/Companies.json", function( data ){
-        console.log('company data:', data);
+    //import company info
+    let companyArr = $.getJSON("scripts/Companies.json", function( obj ){
+        console.log('company data:', obj);
+
+        $.each(obj, function(key, value) {
+            console.log('hello', value);
+            let optionId = 'company' + value.id;
+            $("#companyDropDown").append(`<option id="${optionId}" data=${value}>${value.company}</option>`)
+        });
     });
 
+
+    //import message template
     let messageTempObj = $.getJSON("scripts/MessageTemplate.json", function( data ){
         console.log('message template:', data);
     });
 
-    console.log(guestsArr);
-    console.log(companiesArr);
-    console.log(messageTempObj);
+    //calculate time of day
+    const currentTime = new Date().getTime();
+
+    let timeFormat = moment(1486852373 * 1000).format('LLL');
+
+    console.log('current time:', timeFormat);
 });
 
 
