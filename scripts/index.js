6a111e107e1f3577f17314bd2fcb127d529b2706
PowerPlayHockey <polarishockey@gmail.com>
2018-11-19 11:25:29 -0600

    importing companies arr and message template file

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,21 @@ console.log('js');
 $(document).ready(function() {
 
 
-    let guestsArr = $.getJSON("scripts/Guests.json", function( data){
+    let guestsArr = $.getJSON("scripts/Guests.json", function( data ){
         console.log('guest data:', data);
     });
 
+    let companiesArr = $.getJSON("scripts/Companies.json", function( data ){
+        console.log('company data:', data);
+    });
+
+    let messageTempObj = $.getJSON("scripts/MessageTemplate.json", function( data ){
+        console.log('message template:', data);
+    });
+
     console.log(guestsArr);
+    console.log(companiesArr);
+    console.log(messageTempObj);
 });
 
 
